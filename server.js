227394990a34fd7bac95658c4ca5c1d019d28ecb
vspath800 <krackermal441@gmail.com>
2024-08-15 07:25:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest=function(request,response){
 	response.writeHead(200);
-	response.end("<h1> Welcome to cloud build</h1>");
+	response.end("<h1> Welcome to cloud build CICD GCR</h1>");
 }
 var www=http.createServer(handleRequest);
 www.listen(8080);
